27f441c7bb1dc3925801944b2b391135af760859
doannguyen263 <doannguyen263@gmail.com>
2019-11-22 09:48:19 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 (function($) {
+
   $.fn.dntheme_search = function(options) {
     var _this = this;
     var settings = $.extend({
@@ -7,40 +8,50 @@
 
     this.each(function(index, value){
       _self =  $(this)
-      var dnsearch_results = $( "<div id='dnsearch_results"+index+"' class='dnsearch_results' data-dnsearch='dnsearch-"+index+"''>Loading ...</div>" )
+      var dnsearch_results = $( "<div id='dnsearch_results"+index+"' class='dnsearch_results dnsearch-"+index+"' data-dnsearch-id='dnsearch-"+index+"''>Loading ...</div>" )
       box_create(dnsearch_results,_self,index)
+      _self.attr('data-dnsearch','dnsearch-'+index);
 
-      _self.keyup(function(e) {
-        dnsearch_results.show()
-        e.stopPropagation();
-        var key_search = $(this).val();
-        if(key_search.length > settings.length_search){
-          var postType = $(this).attr('data-postType')
-          box_search(dnsearch_results,postType,key_search);
-        }
+      var x_timer; 
+      _self.on( "change keydown input",function(e) {
+        _self_this = $(this)
+        dnsearch_results.show();
+        var key_search = _self_this.val();
+        clearTimeout(x_timer);
+          x_timer = setTimeout(function(e) {
+          if(key_search.length > settings.length_search){
+            var query = _self_this.attr('data-query')
+            box_search(dnsearch_results,query,key_search);
+          }else{
+            dnsearch_results.hide().empty();
+          }
+        }, 1000);
+
       });
+
       box_close(dnsearch_results);
     });
+
+    $('.dnsearch_results').on( "click",'.item__wrap',_box_select); // Select item
     
     function box_create(dnsearch_results,_this,index) {
       var box_offset = _this.offset();
-        var box_height = _this.innerHeight();
-        var box_width = _this.outerWidth();
+      var box_height = _this.innerHeight();
+      var box_width = _this.outerWidth();
       
       $('body').addClass('dnsearch--active')
-      create_box = dnsearch_results.addClass('tez'+index).insertAfter('body')
-      dnsearch_results.css({"position":"absolute","top":box_offset.top + box_height,'left':box_offset.left,'width':box_width})
+      create_box = dnsearch_results.insertAfter('body')
+      dnsearch_results.css({"position":"absolute","top":box_offset.top + box_height,'left':box_offset.left,'width':box_width,"z-index":999})
 
     }
-    function box_search(dnsearch_results,postType,key_search) {
-      console.log(postType)
+    function box_search(dnsearch_results,query,key_search) {
       $.ajax({
-            type : 'post',
+            type : 'POST',
             url : dntheme_params.ajax_url,
             data : {
                 action : 'dnsearch_ajax',
                 data : key_search,
-                postType : postType
+                query : query
             },
             success : function( data ) {
               dnsearch_results.html(data)
@@ -54,10 +65,16 @@
     // Close box
     $(this).click(function(e){
       e.stopPropagation();
-      // dnsearch_results.empty();
-      console.log($(this))
       $(this).show();
     });
+
+    function _box_select() {
+      dnsearch_id = $(this).parents('.dnsearch_results').attr('data-dnsearch-id');
+      option_val = $(this).text().trim();
+      $("[data-dnsearch="+dnsearch_id+"]").val(option_val).change();
+      box_close($(this));
+      }
+
     function box_close(_this) {
       $(document).click(function(e){
         $('body').removeClass('dnsearch--active')
